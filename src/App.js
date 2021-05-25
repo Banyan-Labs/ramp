8f53934b205c534a9aa01f0e63de42bd8a111005
Austin Hill <Austinkinghill@gmail.com>
2021-05-25 11:33:21 -0700

    Added MyJumbo component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
-import {Jumbotron, Button} from 'react-bootstrap';
 import Footer from './Footer';
+import MyJumbo from './components/MyJumbo';
 
 function App() {
   return (
     <>
     <div className="App">
-     <Navigation/>
-     <Jumbotron>
-        <h1>Hello, world!</h1>
-        <p>
-           Welcome to our Ramp Team Project
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>
+      <Navigation/>
+      <MyJumbo/>
+      <Footer/>
     </div>
-    <Footer/>
     </>
   );
 }
