8fb485c1900ae9f951057ce84b5ff33756c8881d
David <jlyles@banyanlabs.io>
2021-05-25 09:57:40 -0500

    making progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation';
+import {Jumbotron, Button} from 'react-bootstrap';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      Hello Ramp Team
+     <Navigation/>
+     <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+           Welcome to our Ramp Team Project
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
     </div>
   );
 }
