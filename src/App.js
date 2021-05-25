04b17f430711c8a9ece3a94c9ae13d749225ab8c
D-Williams2020 <83662578+smarino130081@users.noreply.github.com>
2021-05-25 13:03:11 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
+    <>
     <div className="App">
       Hello Ramp Team
     </div>
+    <Footer/>
+    </>
   );
 }
 
