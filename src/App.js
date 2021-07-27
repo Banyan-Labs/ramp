5455c32c2b17d4614464cb7360699955a6338089
Sarahunt89 <87195235+Sarahunt89@users.noreply.github.com>
2021-07-27 11:47:10 -0700

    RAMP-50: Make Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello Ramp Team
+
+      <Navbar />
+      
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+
+const Navbar = () => {
+    return (
+        <div id="navbar">
+
+        </div>
+     );
+}
+ 
+export default Navbar;
