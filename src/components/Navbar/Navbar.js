cf2b7fdbe951c3eda7d0bf4371b752d8b24c5415
Sarahunt89 <87195235+Sarahunt89@users.noreply.github.com>
2021-07-27 14:04:35 -0700

    RAMP-50: Made Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,24 @@ import './style.css';
 
 const Navbar = () => {
     return (
-        <div id="navbar">
+        <div id="rectangle1">
 
+            {/* LEFT SIDE OF NAVBAR*/}
+            <div id="providerDirectory">
+                <p>Provider Directory</p>
+            </div>
+
+            {/* RIGHT SIDE OF NAVBAR*/}
+            <div>
+                <a href="#" id="learnMore">Learn More</a>
+                <a href="#" id="pricing">Pricing</a>
+                <a href="#" id="login">Login</a>
+            </div>
+
+        {/* CLOSING DIV FOR #RECTANGLE1*/}
         </div>
-     );
+
+    );
 }
- 
+
 export default Navbar;
