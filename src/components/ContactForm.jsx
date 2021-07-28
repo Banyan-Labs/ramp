3421372ffbd63f891ee26bf6df31b17b708ccdea
Adrian Diaz <adiaz@banyanlabs.io>
2021-07-28 09:26:04 -0700

    added Contact Form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,22 @@
+import "./contact-form.css";
+
+const ContactForm = () => {
+  return (
+    <div className="contact-container ">
+      <form>
+        <label className="contact-header">Contact Us</label>
+        <div className="contact-name">
+          <input type="text" name="firstname" placeholder="First Name" />
+          <input type="text" name="lastname" placeholder="Last Name" />
+        </div>
+        <div className="contact-info">
+          <input type="email" name="email" placeholder="Email" />
+          <textarea name="message" id="" cols="30" rows="4" placeholder="Message..."></textarea>
+          <input type="submit" value="Send" className="contact-button" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
