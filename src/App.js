0df0f108b829721af9195c22ee834563e58618e9
Shandea <86807815+Shandea@users.noreply.github.com>
2021-07-28 09:04:39 -0700

    created footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       Hello Ramp Team
+      <Footer />
     </div>
   );
 }
