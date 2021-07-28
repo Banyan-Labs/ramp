bffa1642362ad4316b721de6e5e1939a08ad7df5
jarvisd86 <86847293+jarvisd86@users.noreply.github.com>
2021-07-28 05:01:11 -0700

    update featured cards

---
+++ src/components/FeaturedCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const FeaturedCard = () => {
+    return (
+        <div class="card-wrapper">
+
+        <header class="container">
+          <h1>Header</h1>
+        </header>
+        
+        <div class="container">
+          <p>Lorem ipsum...</p>
+        </div>
+        
+        <footer class="container ">
+          <h5>Footer</h5>
+        </footer>
+        
+        </div>
+    )
+}
+
+export default FeaturedCard
