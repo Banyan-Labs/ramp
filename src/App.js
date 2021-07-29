afdbeb15309dd2bab5ad6a351bf6273e00cf2f85
StephanieKirshner <88007877+StephanieKirshner@users.noreply.github.com>
2021-07-29 08:26:02 -0700

    Added Blurb Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ContactForm from './components/ContactForm';
+import Blurb from './components/Blurb/Blurb';
+// import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className="App">
       Hello Ramp Team
-      <ContactForm/>
+      <Blurb/>
+      {/* <ContactForm/> */}
     </div>
   );
 }
---
+++ src/components/Blurb/Blurb.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Blurb.css'   
+
+const Blurb = () => {
+    return (
+        <div className="blurbContainer">
+        <div className="blurb"></div>
+        <div className="blurb"></div>
+        <div className="blurb"></div>
+        </div>
+    )
+}
+
+export default Blurb;
