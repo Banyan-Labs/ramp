ce5d7102cf7d29062dfdcd919d27a24a046ef0cf
Sarahunt89 <87195235+Sarahunt89@users.noreply.github.com>
2021-07-29 11:13:34 -0700

    RAMP-50: Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,10 +12,26 @@ const Navbar = () => {
             </div>
 
             {/* RIGHT SIDE OF NAVBAR*/}
-            <div>
+            <div id="links">
                 <a href="#" id="learnMore">Learn More</a>
                 <a href="#" id="pricing">Pricing</a>
                 <a href="#" id="login">Login</a>
+            </div>
+
+            <div className="dropdown">
+                <i className="fa fa-bars fa-2x"></i>
+
+            {/* LINKS INSIDE THE DROPDOWN BOX*/}
+                <div className="dropdown-content">
+                    <li >
+                    <a href="#">Learn More</a>
+                    <br></br>
+                    <a href="#">Pricing</a>
+                    <br></br>
+                    <a href="#">Login</a>
+                    </li>
+                </div>
+
             </div>
 
         {/* CLOSING DIV FOR #RECTANGLE1*/}
